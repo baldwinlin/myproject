2c3848c9df9c54892c4f7ac7f4e5ec190ed1dd5f
BaldwinLin <baldwin.c.lin@gmail.com>
2021-02-27 17:37:00 +0800

    Java data type

--- src/main/java/com/win/Hello.java
+++ src/main/java/com/win/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         Person p = new Person();
         p.hello();
+        int age = 50;
+        float height = 1.63f;
 //        System.out.println("Hello World");
     }
 }
