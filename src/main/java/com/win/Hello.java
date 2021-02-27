fbd38356ca7f089b7585b954ac8d352cdba25217
unknown <baldwin.c.lin@gmail.com>
2021-02-27 17:06:01 +0800

    Initial commit

---
+++ src/main/java/com/win/Hello.java
@@ -0,0 +1,9 @@
+package com.win;
+
+public class Hello {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.hello();
+//        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/com/win/Person.java
@@ -0,0 +1,7 @@
+package com.win;
+
+public class Person {
+    public void hello() {
+        System.out.println("Hello world");
+    }
+}
